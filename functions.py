02d27c1f3c36a83ce4f58ef1c0dececc2e24ceaa
jesarx <eduardop@riseup.net>
2024-04-18 17:14:27 -0600

    Added more stuff

--- functions.py
+++ functions.py
@@ -1,31 +1,51 @@
+from cryptos import sha256, random_key, Bitcoin
 import os
 
-def loadWallets(NumberOfLines=10):
+b = Bitcoin()
+
+
+def loadWallets():
+    limit = int(input("Enter the number of wallets to load in millions: "))
+
     wallets = set()
-    linesInMillions = NumberOfLines * 1000000
-    
-    if not os.path.exists('blockchair_bitcoin_addresses_and_balance_LATEST.tsv'):
-        print('Downloading wallet list...')
-        os.system('wget http://addresses.loyce.club/blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz')
-        print('Uncompressing wallet list...')
-        os.system('tar -xzf blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz')
-        os.system('rm blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz')
-    
-    print('Loading wallets...')    
-    with open('blockchair_bitcoin_addresses_and_balance_LATEST.tsv', 'r') as file:
-        for _ in range(linesInMillions):  
-            line = file.readline().strip() 
-            if not line:  
+    linesInMillions = limit * 1000000
+
+    if not os.path.exists("blockchair_bitcoin_addresses_and_balance_LATEST.tsv"):
+        print("Downloading wallet list...")
+        os.system(
+            "wget http://addresses.loyce.club/blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz"
+        )
+        print("Uncompressing wallet list...")
+        os.system("gunzip blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz")
+
+    print("Loading wallets...")
+    with open("blockchair_bitcoin_addresses_and_balance_LATEST.tsv", "r") as file:
+        for _ in range(linesInMillions):
+            line = file.readline().strip()
+            if not line:
                 break
-            wallet, _ = line.split('\t') 
+            wallet, _ = line.split("\t")
             wallets.add(wallet)
-    os.system('clear')
+    os.system("clear")
     return wallets
 
+
 def writeFoundKeys(pKeysFound):
-    with open('foundKeys.txt', 'w') as file:
+    with open("foundKeys.txt", "w") as file:
         for pair in pKeysFound:
-            file.write(str(pair) + '\n')
-            
+            file.write(str(pair) + "\n")
+
     return None
-  
+
+
+def genWallet(phrase=None):
+    if phrase:
+        pkey = sha256(phrase)
+    else:
+        pkey = random_key()
+
+    segwit = b.privtosegwitaddress(pkey)
+    p2pkh = b.privtop2pkh(pkey)
+    p2sh = b.privtop2wpkh_p2sh(pkey)
+
+    return [pkey, segwit, p2pkh, p2sh]
--- main.py
+++ main.py
@@ -1,35 +1,76 @@
-from cryptos import random_key, Bitcoin
-import time
+from cryptos import Bitcoin
 import os
 import functions as f
+
 b = Bitcoin()
 
-# VARIABLES
-RANGE = 10 # Number of wallet to load in millions. Wallets are ordered by balance. The wallets with the most balance are at the top.
-SEARCHES = 15 # Number of random bitcoin addresses to generate and search per second.
-
-
-wallets = f.loadWallets(RANGE)
-
-pKeysFound = []
-count = 0
+print("This is a BTC wallet finder and generator")
+print("Select the desired option:")
+print("""
+    1. Download wallets with balance and try to generate key pairs for any of this wallets.
+    2. Search for brainwallets given a certain file with phrases.
+    3. Generate a new wallet.
+    4. Exit.
+""")
 
 while True:
-    for _ in range(SEARCHES):    
-        key = random_key()
-        segwitAddress = b.privtosegwitaddress(key)
-        address = b.privtoaddr(key)
-    
-        if segwitAddress in wallets or address in wallets:
-            pair = {key: [segwitAddress, address]}
-            pKeysFound.append(pair)            
-            f.writeFoundKeys(pKeysFound)
-                    
-        count += 1
-  
-    os.system('clear')
-    print(f'{count:,} bitcoin adresses searched')
-    
-    if pKeysFound:
-      print(f'{len(pKeysFound)} wallets found: {pKeysFound} "\n"')
-    time.sleep(1)
+    userAction = input("Enter the desired number option: ")
+
+    if userAction == "1":
+        wallets = f.loadWallets()
+        pKeysFound = []
+        count = 0
+
+        while True:
+            randWallet = f.genWallet()
+
+            if (
+                randWallet[1] in wallets
+                or randWallet[2] in wallets
+                or randWallet[3] in wallets
+            ):
+                pKeysFound.append(randWallet)
+                f.writeFoundKeys(pKeysFound)
+
+            count += 1
+
+            os.system("clear")
+            print(
+                f"{count:,} bitcoin adresses searched, {len(pKeysFound)} private keys with balance found."
+            )
+
+    elif userAction == "2":
+        os.system("clear")
+        wallets = f.loadWallets()
+        fileName = input(
+            "Enter the name of the file with the phrases (must be in root dir): "
+        )
+        passwords = set()
+        count = 0
+
+        with open(fileName, "r") as file:
+            for line in file:
+                passwords.add(line.strip())
+
+        for password in passwords:
+            randWallet = f.genWallet(password)
+
+            if (
+                randWallet[1] in wallets
+                or randWallet[2] in wallets
+                or randWallet[3] in wallets
+            ):
+                f.writeFoundKeys(
+                    f"Brainwallet: {password} \nPrivate Key: {randWallet[0]} \nSegwit Address: {randWallet[1]} \np2pkh Address: {randWallet[2]} \n p2sh Address: {randWallet[3]} \n\n"
+                )
+            count += 1
+            os.system("clear")
+            print(f"{count:,} passwords tried.")
+    elif userAction == "3":
+        randWallet = f.genWallet()
+        print(
+            f"Private Key: {randWallet[0]} \nSegwit Address: {randWallet[1]} \np2pkh Address: {randWallet[2]} \n p2sh Address: {randWallet[3]} \n\n"
+        )
+
+    elif userAction == "4":
+        exit()
